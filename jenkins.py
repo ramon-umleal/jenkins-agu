cee531313a0e8871ced80423c0daa838b24033a0
ramon.leal <ramon.leal@agu.gov.br>
2024-03-08 20:44:16 -0300

    jenkinsci.plugins.pipeline.modeldefinition.actions.DeclarativeJobAction

--- jenkins.py
+++ jenkins.py
@@ -7,7 +7,7 @@ def conectar_jenkins(jenkins_url, jenkins_user, jenkins_token):
     try:
         session = requests.Session()
         session.auth = (jenkins_user, jenkins_token)
-        session.headers.update({'Content-Type': 'application/json'})
+        session.headers.update({"Content-Type": "application/json"})
         # Testa a conexão obtendo informações do Jenkins
         response = session.get(f"{jenkins_url}/api/json")
         response.raise_for_status()  # Lança uma exceção se a resposta não for bem-sucedida
@@ -17,53 +17,67 @@ def conectar_jenkins(jenkins_url, jenkins_user, jenkins_token):
         print(f"Erro ao conectar-se ao Jenkins: {e}")
         return None
 
+
 def criar_pipeline(session, nomeAplicacao, jenkins_url):
     pipeline_name = f"{nomeAplicacao}-pipeline"
     pipeline_config_xml = f"""
-        <flow-definition plugin="workflow-job@2.40">
-            <actions/>
-            <description></description>
-            <keepDependencies>false</keepDependencies>
-            <properties/>
-            <definition class="org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition" plugin="workflow-cps@2.89">
-                <scm class="hudson.plugins.git.GitSCM" plugin="git@4.11.0">
-                    <configVersion>2</configVersion>
-                    <userRemoteConfigs>
-                        <hudson.plugins.git.UserRemoteConfig>
-                            <url>https://github.com/agu-pgu/stj-link.git</url>
-                            <credentialsId>jenkins-token-git-privado</credentialsId>
-                        </hudson.plugins.git.UserRemoteConfig>
-
-                    </userRemoteConfigs>
-                    <branches>
-                        <hudson.plugins.git.BranchSpec>
-                            <name>*/DEVOP</name>
-                        </hudson.plugins.git.BranchSpec>
-                    </branches>
-                    <doGenerateSubmoduleConfigurations>false</doGenerateSubmoduleConfigurations>
-                    <submoduleCfg class="list"/>
-                    <extensions/>
-                </scm>
-                <scriptPath>Jenkinsfile</scriptPath>
-            </definition>
-            <triggers/>
-            <disabled>false</disabled>
-        </flow-definition>
+     <flow-definition plugin="workflow-job@1385.vb_58b_86ea_fff1">
+    <actions/>
+    <description>Teste de criação job no Jenkins {nomeAplicacao}</description>
+    <keepDependencies>false</keepDependencies>
+    <properties>
+        <org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty>
+            <triggers>
+                <hudson.triggers.SCMTrigger>
+                    <spec>H/20 * * * *</spec>
+                    <ignorePostCommitHooks>false</ignorePostCommitHooks>
+                </hudson.triggers.SCMTrigger>
+            </triggers>
+        </org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty>
+    </properties>
+    <definition class="org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition" plugin="workflow-cps@3880.vb_ef4b_5cfd270">
+        <scm class="hudson.plugins.git.GitSCM" plugin="git@5.2.1">
+            <configVersion>2</configVersion>
+            <userRemoteConfigs>
+                <hudson.plugins.git.UserRemoteConfig>
+                    <url>https://github.com/agu-pgu/stj-link.git</url>
+                    <credentialsId>jenkins-token-git-privado</credentialsId>
+                </hudson.plugins.git.UserRemoteConfig>
+            </userRemoteConfigs>
+            <branches>
+                <hudson.plugins.git.BranchSpec>
+                    <name>*/DEVOP</name>
+                </hudson.plugins.git.BranchSpec>
+            </branches>
+            <doGenerateSubmoduleConfigurations>false</doGenerateSubmoduleConfigurations>
+            <gitTool>default</gitTool>
+            <submoduleCfg class="empty-list"/>
+            <extensions/>
+        </scm>
+        <scriptPath>Jenkinsfile</scriptPath>
+        <lightweight>false</lightweight>
+    </definition>
+    <triggers/>
+    <disabled>false</disabled>
+</flow-definition>
     """
     try:
         response = session.post(
             f"{jenkins_url}/createItem?name={pipeline_name}",
             headers={"Content-Type": "application/xml"},
-            data=pipeline_config_xml
+            data=pipeline_config_xml,
         )
         if response.status_code == 200:
             print(f"Pipeline '{pipeline_name}' criado com sucesso!")
         elif response.status_code == 400:
-            print(f"Já existe um pipeline com o nome '{pipeline_name}' no Jenkins porfavor escolher  outro nome.")
+            print(
+                f"Já existe um pipeline com o nome '{pipeline_name}' no Jenkins porfavor escolher  outro nome."
+            )
         else:
             response.raise_for_status()
     except Exception as e:
         print(f"Falha ao criar o pipeline '{pipeline_name}' no Jenkins: {e}")
+
 
 # Exemplo de uso
 def main():
@@ -76,6 +90,6 @@ def main():
     if session:
         criar_pipeline(session, nomeAplicacao, jenkins_url)
 
+
 if __name__ == "__main__":
     main()
-                    
